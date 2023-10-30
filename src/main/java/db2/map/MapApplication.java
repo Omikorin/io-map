6892b6e082be1ac9994d6fb81e49095cfbe96f04
Andrii Zhukov <262701@student.pwr.edu.pl>
2023-10-30 14:13:17 +0100

    feat/Initializing Project

---
+++ src/main/java/db2/map/MapApplication.java
@@ -0,0 +1,36 @@
+package db2.map;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.sql.*;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class MapApplication {
+
+	public static Connection cn;
+
+	public static void connect() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			cn = DriverManager.getConnection(
+					"jdbc:postgresql://localhost:5432/", "postgres", "password");
+			if (cn != null) {
+				System.out.println("Connection successfull");
+			} else {
+				System.out.println("Connection failed");
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		/* Start app */
+		connect();
+	}
+}
---
+++ src/test/java/db2/map/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package db2.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
